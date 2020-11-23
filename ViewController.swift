8e4e996c05c999ce84edd0a8c0ac7e12da092d6c
Abdon Morales <39339393+Server2356@users.noreply.github.com>
2020-11-23 13:37:28 -0600

    Adds View Controller.swift for controlling NewsUI

---
+++ ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  GEO
+//
+//  Created by Abdon Morales Jr on 11/20/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        func request(){}
+        do {
+            _ = NSURL(string: "https://google.com")
+        }
+    }
+
+
+}
+
